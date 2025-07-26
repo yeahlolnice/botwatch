fb224cb1ca345fe0124bf4c1bbf8268cb6d82270
yeahlolnice <mattwilly98@hotmail.com>
2025-07-26 13:06:29 +1000

    added back in .csproj file to git

--- Botwatch.xyz/obj/Debug/net8.0/Botwatch.xyz.AssemblyInfo.cs
+++ Botwatch.xyz/obj/Debug/net8.0/Botwatch.xyz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Botwatch.xyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f196fbaaa91356e9f954754d285267286110c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Botwatch.xyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Botwatch.xyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
